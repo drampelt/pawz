63d03743208c42b6c14a1b017b7b34b738d0445d
Colin Evans <Colin@Colins-MacBook-Pro-2.local>
2017-10-21 00:35:56 -0400

    added photo functionality

--- app/src/main/java/io/hackharvard/pawz/PetInfoActivity.kt
+++ app/src/main/java/io/hackharvard/pawz/PetInfoActivity.kt
@@ -1,18 +1,32 @@
 package io.hackharvard.pawz
 
+import android.content.ComponentName
 import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Vibrator
+import android.provider.MediaStore
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.View
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_pet_info.*
+import pl.aprilapps.easyphotopicker.EasyImage
+import pl.aprilapps.easyphotopicker.DefaultCallback
+import java.io.File
+
 
 class PetInfoActivity : AppCompatActivity() {
     private var marker: Marker? = null
+
+    companion object {
+        private val ALBUM_IMAGE = 0
+        private val PHOTO_IMAGE = 1
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,6 +52,13 @@ class PetInfoActivity : AppCompatActivity() {
                 )
                 vibrationService.vibrate(25)
             }
+        }
+
+        choosePhotoButton.setOnClickListener {
+            val stringTxt = "Take or Choose an Image"
+            EasyImage.openChooserWithGallery(this, stringTxt, 0);
+
+            petPicture
         }
     }
 
@@ -81,4 +102,21 @@ class PetInfoActivity : AppCompatActivity() {
         menuInflater.inflate(R.menu.menu_add_lost_pet, menu)
         return true
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        EasyImage.handleActivityResult(requestCode, resultCode, data, this, object : DefaultCallback() {
+            override fun onImagePickerError(e: Exception?, source: EasyImage.ImageSource?, type: Int) {
+                //Some error handling
+            }
+
+            override
+            fun onImagesPicked(imagesFiles: List<File>, source: EasyImage.ImageSource, type: Int) {
+                Picasso.with(this@PetInfoActivity).load(imagesFiles[0]).into(petPicture)
+            }
+        })
+    }
+
+
 }
